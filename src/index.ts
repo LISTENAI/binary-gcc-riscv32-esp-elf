2b5a7ce4d853ef88dc1416dc3478ce79aaf5e502
xychen <xychen@listenai.com>
2021-11-09 17:21:59 +0800

    Fix homeDir

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ const execFile = promisify(_execFile);
 export const HOME = join(__dirname, '..', 'binary');
 
 export default <Binary>{
-  homeDir: HOME,
+  homeDir: join(HOME, 'riscv32-esp-elf'),
 
-  binaryDir: join(HOME, 'bin'),
+  binaryDir: join(HOME, 'riscv32-esp-elf', 'bin'),
 
   async version() {
     const { stdout } = await execFile(join(this.binaryDir, 'riscv32-esp-elf-gcc'), ['--version']);
